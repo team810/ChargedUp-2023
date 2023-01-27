7ce004dac1bd3f402112c2d74fb885f54e330fb8
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2023-01-27 16:30:16 -0500

    joystick config

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,10 +28,10 @@ public class RobotContainer {
    private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
    private final Limelight m_lime = new Limelight();
 
-  // private final Joystick RIGHT = new Joystick(0);
+  private final Joystick RIGHT = new Joystick(0);
   private final Joystick LEFT = new Joystick(1);  
   //private final PathPlannerTrajectory Path = PathPlanner.loadPath("New Path",4,3);
-  private XboxController mController = new XboxController(0);
+  // private XboxController mController = new XboxController(0);
 
   public RobotContainer() {
     // Set up the default command for the drivetrain.
@@ -40,19 +40,19 @@ public class RobotContainer {
     // Left stick X axis -> left and right movement
     // Right stick X axis -> rotation
     // modifyAxis(0);
-    // m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
-    //     m_drivetrainSubsystem,
-    //     ()-> -modifyAxis(RIGHT.getY()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-    //     ()-> -modifyAxis(RIGHT.getX()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-    //     ()-> -modifyAxis(LEFT.getZ()) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND));
+    m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+        m_drivetrainSubsystem,
+        ()-> -modifyAxis(RIGHT.getY()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+        ()-> -modifyAxis(RIGHT.getX()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+        ()-> -modifyAxis(LEFT.getZ()) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND));
     
 
         //XboxControllera Controlls 
-        m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
-        m_drivetrainSubsystem,
-        ()-> -modifyAxis(mController.getRawAxis(5)) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-        ()-> -modifyAxis(mController.getRawAxis(4)) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-        ()-> -modifyAxis(mController.getRawAxis(0)) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND));
+        // m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+        // m_drivetrainSubsystem,
+        // ()-> -modifyAxis(mController.getRawAxis(5)) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+        // ()-> -modifyAxis(mController.getRawAxis(4)) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+        // ()-> -modifyAxis(mController.getRawAxis(0)) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND));
       
     // Configure the button bindings
     configureButtonBindings();
@@ -75,18 +75,18 @@ public class RobotContainer {
     //Processing
     // new JoystickButton(LEFT, 1).onTrue(new InstantCommand(()-> m_lime.setMode(2)));
     //Zeros the gyroscope
-    // new JoystickButton(RIGHT, 1).onTrue(new InstantCommand(m_drivetrainSubsystem::zeroGyroscope));
+    new JoystickButton(RIGHT, 1).onTrue(new InstantCommand(m_drivetrainSubsystem::zeroGyroscope));
     
     //Xboxcontroller Controlles 
     // Back button zeros the gyroscope on Xboxcontroller
     //new JoystickButton(mController, 5).onTrue(new InstantCommand(m_drivetrainSubsystem::zeroGyroscope));
     //Limelight buttons on Xboxcontroller
     //Apirltag
-    new JoystickButton(mController, 1).onTrue(new InstantCommand(()-> m_lime.setMode(0)));
+    // new JoystickButton(mController, 1).onTrue(new InstantCommand(()-> m_lime.setMode(0)));
     //Limelight 
-    new JoystickButton(mController, 2).onTrue(new InstantCommand(()-> m_lime.setMode(1)));
+    // new JoystickButton(mController, 2).onTrue(new InstantCommand(()-> m_lime.setMode(1)));
     //Processing 
-    new JoystickButton(mController, 3).onTrue(new InstantCommand(()-> m_lime.setMode(2)));
+    // new JoystickButton(mController, 3).onTrue(new InstantCommand(()-> m_lime.setMode(2)));
     }
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
