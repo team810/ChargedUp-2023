c10544ad64afeb25e749c5d7361cd599ae97d9d7
Matthew Lomando <m.lomando@outlook.com>
2023-02-15 07:38:02 -0500

    Jack commit for the night

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,24 +4,27 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Drivetrain;
 
 public class RobotContainer {
-//   private final Drivetrain m_drivetrainSubsystem = new Drivetrain();
+
+  private final Drivetrain m_drivetrainSubsystem = new Drivetrain();
 //   private final Gripper m_gripper = new Gripper();
-//   private final Conveyor m_conveyor = new Conveyor();
+//   private final Conveyor conveyor = new Conveyor();
 //   private final Intake m_intake = new Intake();
   private final Arm arm = new Arm();
 
   public RobotContainer() {
 
     // Set up the default command for the drivetrain.
-//    m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
-//            m_drivetrainSubsystem,
-//            () -> modifyAxis(OIConstants.DRIVE_GAMEPAD.getLeftX()) * DrivetrainConstants.MAX_VELOCITY_METERS_PER_SECOND,
-//            () -> modifyAxis(OIConstants.DRIVE_GAMEPAD.getLeftY()) * DrivetrainConstants.MAX_VELOCITY_METERS_PER_SECOND,
-//            () -> modifyAxis(OIConstants.DRIVE_GAMEPAD.getRightX())* DrivetrainConstants.MAX_VELOCITY_METERS_PER_SECOND
-//    ));
+    m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+            m_drivetrainSubsystem,
+            () -> modifyAxis(Constants.OIConstants.DRIVE_GAMEPAD.getLeftX()) * DrivetrainConstants.MAX_VELOCITY_METERS_PER_SECOND,
+            () -> modifyAxis(Constants.OIConstants.DRIVE_GAMEPAD.getLeftY()) * DrivetrainConstants.MAX_VELOCITY_METERS_PER_SECOND,
+            () -> modifyAxis(Constants.OIConstants.DRIVE_GAMEPAD.getRightX())* DrivetrainConstants.MAX_VELOCITY_METERS_PER_SECOND
+    ));
 
     // Configure the button bindings
     configureButtonBindings();
@@ -51,15 +54,14 @@ public class RobotContainer {
 //    );
 
 
+    new Trigger(Constants.OIConstants.DRIVE_GAMEPAD::getXButton).whileTrue(new InstantCommand(arm::rest));
 
     new Trigger(Constants.OIConstants.DRIVE_GAMEPAD::getAButton).whileTrue(new InstantCommand(arm::lowGoal));
-    new Trigger(Constants.OIConstants.DRIVE_GAMEPAD::getBButton).whileTrue(new InstantCommand(arm::rest));
-
-
+    new Trigger(Constants.OIConstants.DRIVE_GAMEPAD::getBButton).whileTrue(new InstantCommand(arm::middleGoal));
+    new Trigger(Constants.OIConstants.DRIVE_GAMEPAD::getYButton).whileTrue(new InstantCommand(arm::highGoal));
 
 
   }
-
   private static double deadband(double value, double deadband) {
     if (Math.abs(value) > deadband) {
       if (value > 0.0) {
