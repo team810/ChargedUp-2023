82014ab362d982510e294f6a1e513f85cd9322e0
Matthew Lomando <m.lomando@outlook.com>
2023-02-26 21:08:02 -0500

    Who the hell has there indent at 2 spaces. I will find you

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -11,67 +11,66 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
-  private final CANSparkMax leftIntakeMotor, rightIntakeMotor;
-  private final DoubleSolenoid piston;
-  private final ShuffleboardLayout INTAKE_VALUES = IntakeConstants.INTAKE_VALUES;
-  private final PneumaticHub pneumaticHub;
+    private final CANSparkMax leftIntakeMotor, rightIntakeMotor;
+    private final DoubleSolenoid piston;
+    private final ShuffleboardLayout INTAKE_VALUES = IntakeConstants.INTAKE_VALUES;
+    private final PneumaticHub pneumaticHub;
 
 
-  /** Creates a new Intake. */
-  public Intake() {
-    leftIntakeMotor = new CANSparkMax(IntakeConstants.LEFT_INTAKE_MOTOR, MotorType.kBrushless);
-    rightIntakeMotor = new CANSparkMax(IntakeConstants.RIGHT_INTAKE_MOTOR, MotorType.kBrushless);
+    /**
+     * Creates a new Intake.
+     */
+    public Intake() {
+        leftIntakeMotor = new CANSparkMax(IntakeConstants.LEFT_INTAKE_MOTOR, MotorType.kBrushless);
+        rightIntakeMotor = new CANSparkMax(IntakeConstants.RIGHT_INTAKE_MOTOR, MotorType.kBrushless);
 
-    pneumaticHub = new PneumaticHub(18);
+        pneumaticHub = new PneumaticHub(18);
 
-    pneumaticHub.clearStickyFaults();
-
-
-    pneumaticHub.enableCompressorDigital();
-
-    piston = pneumaticHub.makeDoubleSolenoid(0, 7);
-
-    shuffleboardInit();
-  }
-
-  public void out()
-  {
-    piston.set(Value.kForward);
-  }
-  public void in()
-  {
-    piston.set(Value.kReverse);
-  }
-
-  public void runIntake()
-  {
-    leftIntakeMotor.set(-IntakeConstants.INTAKE_MOTOR_SPEED);
-    rightIntakeMotor.set(IntakeConstants.INTAKE_MOTOR_SPEED);
-  }
-  public void stopIntake()
-  {
-    leftIntakeMotor.set(0);
-    rightIntakeMotor.set(0);
-  }
-  public void runIntakeReversed()
-  {
-    leftIntakeMotor.set(IntakeConstants.INTAKE_MOTOR_SPEED);
-    rightIntakeMotor.set(-IntakeConstants.INTAKE_MOTOR_SPEED);
-  }
+        pneumaticHub.clearStickyFaults();
 
 
-  public void toggleIntake()
-  {
-    this.piston.toggle();
-  }
+        pneumaticHub.enableCompressorDigital();
 
-  public void shuffleboardInit() {
-    INTAKE_VALUES.addDouble("Velocity", () -> leftIntakeMotor.getEncoder().getVelocity());
+        piston = pneumaticHub.makeDoubleSolenoid(0, 7);
 
-  }
+        shuffleboardInit();
+    }
 
-  public Value getSolenoidValue() {
-    return piston.get();
-  }
+    public void out() {
+        piston.set(Value.kForward);
+    }
+
+    public void in() {
+        piston.set(Value.kReverse);
+    }
+
+    public void runIntake() {
+        leftIntakeMotor.set(-IntakeConstants.INTAKE_MOTOR_SPEED);
+        rightIntakeMotor.set(IntakeConstants.INTAKE_MOTOR_SPEED);
+    }
+
+    public void stopIntake() {
+        leftIntakeMotor.set(0);
+        rightIntakeMotor.set(0);
+    }
+
+    public void runIntakeReversed() {
+        leftIntakeMotor.set(IntakeConstants.INTAKE_MOTOR_SPEED);
+        rightIntakeMotor.set(-IntakeConstants.INTAKE_MOTOR_SPEED);
+    }
+
+
+    public void toggleIntake() {
+        this.piston.toggle();
+    }
+
+    public void shuffleboardInit() {
+        INTAKE_VALUES.addDouble("Velocity", () -> leftIntakeMotor.getEncoder().getVelocity());
+
+    }
+
+    public Value getSolenoidValue() {
+        return piston.get();
+    }
 
 }
